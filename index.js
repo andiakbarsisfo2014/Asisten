919447e0bd583d24a5c730a5435206b9396a0eba
useless-dev <60900114063@uin-alauddin.ac.id>
2019-10-04 16:40:37 +0800

    noob

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import AsistensService from './AsistensService';
-import Other from './src/screens/Praktikum';
+// import Other from './src/screens/Praktikum';
 const AsistenHeadless = async () => {
   // var socket = await SocketIOClient('http://103.55.216.17:3000', {transports: ['websocket'], pingTimeout: 30000});
   // socket.on('new_regist_client', (data) => {
@@ -22,6 +22,12 @@ const AsistenHeadless = async () => {
     // store.dispatch(setHeartBeat(false));
   // }, 1000);
 };
+const Other = () => {
+  console.warn('msg',this.props)
+  return (
+    null
+  )
+}
 AsistensService.startService();
 AppRegistry.registerHeadlessTask('AsistensService', () => AsistenHeadless);
 AppRegistry.registerComponent(appName, () => App);
